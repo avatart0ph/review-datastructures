925488f607bc5ce5c06872966ee9f5ace337cb22
Denise Michelle del Bando <avatart0ph@users.noreply.github.com>
2017-07-09 14:10:29 -0400

    - minor fixes

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -1,6 +1,3 @@
-
-import java.util.*;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,24 +16,11 @@ public class LinkedListMain
         MyLinkedList<String> myList = new MyLinkedList<>();
        
         String[] items = {"6","5","7", "3", "2"};
-//        Node prev = null;
-//        for(int ii = items.length - 1; ii >= 0; ii--)
-//        {
-//            Node newModel = new Node();
-//            newModel.setValue(items[ii]);            
-//            newModel.setNext(prev);
-//            prev = newModel;
-//        }
-//        Node item = prev;
-//        while(item != null)
-//        {   
-//            System.out.println(item.getValue());            
-//            item = item.getNext();
-//        }
 
+        //just add sequentially
         for(String item : items)
         {
-            myList.AddTail(item);        
+            myList.Add(item);        
         }
         System.out.println(myList.toString());
     }
