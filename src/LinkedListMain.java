225be7ef8e29c496b2efa4602f5eed0f2c2c2a84
Denise Michelle del Bando <avatart0ph@users.noreply.github.com>
2017-07-09 15:16:51 -0400

    create RemoveFirst and RemoveLast

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -14,14 +14,33 @@ public class LinkedListMain
     public static void main(String[] args)
     {
         MyLinkedList<String> myList = new MyLinkedList<>();
+        MyLinkedList<String> myList2 = new MyLinkedList<>();
        
-        String[] items = {"6","5","7", "3", "2"};
+        String[] items = {"6","5","7", "1", "3", "2", "1"};
 
         //just add sequentially
         for(String item : items)
         {
             myList.Add(item);        
+            myList2.AddFirst(item);
         }
-        System.out.println(myList.toString());
+        //order
+        System.out.println("First Linked List - " + myList.toString());
+        //reverse order
+        System.out.println("Second Linked List - " + myList2.toString());
+        //remove first item in 2nd linked list
+        System.out.println(myList2.RemoveFirst());
+        //remove last item in 2nd linked list
+        System.out.println(myList2.RemoveLast());
+        //print new linked list
+        System.out.println("Second Linked List - " + myList2.toString());
+        System.out.println("Second Linked List Length - " + myList2.getLength());
+        
+        //remove last twice on first linked list
+        System.out.println(myList.RemoveLast());
+        System.out.println(myList.RemoveLast());
+        //print new linked list
+        System.out.println("First Linked List - " + myList.toString());
+        System.out.println("First Linked List Length - " + myList.getLength());
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -27,27 +27,31 @@ public class MyLinkedList<T>
         return this.tail;
     }
     
-    public int length;
+    private int length;
+    
+    public int getLength(){
+        return this.length;
+    }
     
     
+    //add on first of linkedlst
     public void AddFirst(T newValue)
     {
         Node<T> item = new Node<>();
         item.setValue(newValue);
         if(length == 0)
         {
-            this.head = item;
             this.tail = item;
-        }else
+        }
+        else
         {
             item.setNext(this.head);
-            this.head = item;
         }
+        this.head = item;
         length++;
-    
     }
     
-    
+    //add on last
     public void Add(T newValue)
     {
         Node<T> item = new Node<>();
@@ -55,27 +59,66 @@ public class MyLinkedList<T>
         if(length == 0)
         {
             this.head = item;
-            this.tail = item;
-        }else
+        }
+        else
         {
             this.tail.setNext(item);
-            this.tail = item;
         }
+        this.tail = item;
         length++;
     }
+    
+    //remove first
+    public T RemoveFirst()
+    {
+        Node<T> returnValue = this.head;
+        
+        this.head = this.head.getNext();
+        length--;
+        return returnValue.getValue();
+    }
+    
+    //remove last
+    public T RemoveLast()
+    {
+        Node<T> pointer = this.head;
+        int ctr = 1;
+        while(ctr <= this.length)
+        {
+            //get last
+            if(ctr == this.length - 1)
+            {
+                //update tail
+                this.tail = pointer;
+                this.tail.setNext(null);
+                
+            }
+            if(pointer.getNext() != null){
+                pointer = pointer.getNext();
+            }
+            
+            
+            ctr++;
+        }
+        length--;
+        return pointer.getValue();
+    }
+    
+    
+    
     @Override
     public String toString()
     {
-        String temp = "";
+        String returnValue = "";
         
         Node item = this.head;
         while(item != null)
         {   
-            temp += item.getValue() + " ";            
+            returnValue += item.getValue().toString() + " ";            
             item = item.getNext();
         }
                 
-        return temp;
+        return returnValue;
     
     }
    
